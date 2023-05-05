7ff21c991e0fc841bd7c5e0883cea1c48772c904
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2023-05-05 14:52:48 +0600

    done

---
+++ pages/products/[id].vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Product details {{ id }}</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis id in
+      repellat voluptatem quos quaerat cupiditate! Doloribus magnam laboriosam
+      iusto expedita, rerum dolore accusantium doloremque fugit libero sequi
+      quibusdam saepe.
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
+
+<style lang="scss" scoped></style>
