df7e1af7e8d988b06ad0b2ad96c676157d130271
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2023-05-06 14:57:16 +0600

    done

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <header>
+      <nuxt-link to="/">Nuxt toJo</nuxt-link>
+      <nav>
+        <ul>
+          <li><nuxt-link to="/">Home</nuxt-link></li>
+          <li><nuxt-link to="/products">Product</nuxt-link></li>
+          <li><nuxt-link to="/about">About</nuxt-link></li>
+        </ul>
+        <a href="/about">normal route-- about</a>
+      </nav>
+    </header>
     <h1>Index page</h1>
     <p>
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa possimus
